b8fe0940bac6932cc930aaf7cb965aad84d13d54
Priyankayadav2 <104354982+Priyankayadav2@users.noreply.github.com>
2025-01-31 15:04:11 +0530

    while loop

--- code.cpp
+++ code.cpp
@@ -1,90 +1,29 @@
 #include <iostream>
 using namespace std;
 int main(){
-    //Conditonal statements
-    //if -else
-    // int n=23;
-    // if(n>=0)
-    // {
-    //     cout<<"number is positive";
+    //while loop
 
+
+    int n=1;
+    while(n<=4){
+        cout<<n<<" ";
+        n++;
+    }
+    return 0;
+
+
+    // int n = 20;
+    // int i=1;
+    // while(i<=20){
+    //     cout<<i<<" ";
+    //     i++;
     // } 
-    // else{
-    //     cout<<"number is negative";
-    // }
 
-// check age
-
-    // int age;
-    // cout<<"Enter a age :"<<endl;
-    // cin>>age;
-    // if(age>=18){
-    //     cout<<"eligible";
-    //         }
-    // else{
-    //     cout<<"not eligible";
-    // }
-    // return 0;
     
 
-
-    // check odd and even number
-
-    // int n;
-    // cout<<"enter a number :";
-    // cin>>n;
-    // if(n%2==0){
-    //     cout<<"numbeer is even";
-
-    // }
-    // else{
-    //     cout<<"number is odd";
-    // }
+    return 0;
 
 
-        //if-elseif-else
 
-        // int marks;
-        // cout<<"marks is :";
-        // cin>>marks;
-        // if(marks>=90){
-        //     cout<<"A Grade";
-        // }
-        // else if(marks<90 && marks>=70){
-        //     cout<<"B Grade";
-        // }
-        // else if(marks<70 && marks >=33){
-        //     cout<<"C Grade";
-        // }
-        // else {
-        //     cout<<"F Grade";
-        // }
-
-
-        // to check uppercase and lower case
-
-        // char ch;
-        // cout<<"enter a character :";
-        // cin>>ch;
-        // if(ch>='a' && ch<='z'){
-        //     cout<<"lower case character";
-        // }
-        // else{
-        //     cout<<"upper case character";
-        // }
-        //ascii check type conversion
-        // if(ch>=97 && ch<=122){
-        //     cout<<"lowercase";
-
-        // }
-        // else{
-        //     cout<<"uppercase";
-        // }
-        
-        //ternary operaators
-        
-        int n;
-        cin>>n;
-        cout<<(n>=0?"number is positive":"number is negative");
 }
 
