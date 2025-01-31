3c0ce4f4729e18381f901e52199b3a610647f58c
Priyankayadav2 <104354982+Priyankayadav2@users.noreply.github.com>
2025-01-31 16:27:32 +0530

    nested-loop

--- code.cpp
+++ code.cpp
@@ -2,9 +2,18 @@
 using namespace std;
 int main(){
    //nested loop
-    int x =10;
-    for (int i=1;i<=x;i++){
-        cout<<"#"<<" ";
+    // int x =10;
+    // for (int i=1;i<=x;i++){
+    //     cout<<"#"<<" ";
+    // }
+    for(int i=1;i<=5;i++){
+        int y=10;
+        for(int j=1;j<=y;j++){
+            cout<<"$"<<" ";
+        }
+        cout<<endl;
+        
     }
+
 }
 
