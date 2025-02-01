bbabdf2cd938a7d2d12c644be6b3bc071d30d18d
Priyankayadav2 <104354982+Priyankayadav2@users.noreply.github.com>
2025-02-01 12:27:49 +0530

    pattern

--- code.cpp
+++ code.cpp
@@ -1,19 +1,10 @@
 #include <iostream>
 using namespace std;
 int main(){
-   //nested loop
-    // int x =10;
-    // for (int i=1;i<=x;i++){
-    //     cout<<"#"<<" ";
-    // }
-    for(int i=1;i<=5;i++){
-        int y=10;
-        for(int j=1;j<=y;j++){
-            cout<<"$"<<" ";
-        }
-        cout<<endl;
+
+        cout<<"Patterns"<<endl;
         
     }
 
-}
+
 
