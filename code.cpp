2c471a419ebf2bebc0a9d221bfd5332cc66178e6
Priyankayadav2 <104354982+Priyankayadav2@users.noreply.github.com>
2025-02-01 12:35:09 +0530

    patterns

--- code.cpp
+++ code.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 using namespace std;
 int main(){
+    int n ;
+    cin>>n;
 
-        cout<<"Patterns"<<endl;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cout<<j;
+        }
+        cout<<endl;
+
+    }
         
     }
 
