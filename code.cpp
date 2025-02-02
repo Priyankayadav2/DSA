69da0ad57cce71c5aa55af28e148238eb46c9078
Priyankayadav2 <104354982+Priyankayadav2@users.noreply.github.com>
2025-02-02 17:19:11 +0530

    triangle

--- code.cpp
+++ code.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout<<"helo";
+    int n;
+    cout<<"Enter your number :";
+    cin>>n;
+    // int num=1;
+    // for(int i=0;i<n;i++){
+    //     for(int j=1;j<=(i+1);j++){
+    //         cout<<num<<" ";
+    //         num++;
+    //     }
+    //     cout<<endl;
+    // }
+    // cout<<"After execution current value of number is : "<<num;
+    // return 0;
+    char ch='A';
+    for(int i=0;i<n;i++){
+        for(int j=1;j<=(i+1);j++){
+            cout<<ch<<" ";
+            ch++;
+        }
+        cout<<endl;
+    }
+    cout<<"After execution current value of number is : "<<ch;
+    return 0;
 }
 
 
