6e2f4c45cff08f4dfb26f5ea348d142ec2cfb31a
Priyankayadav2 <104354982+Priyankayadav2@users.noreply.github.com>
2025-02-02 17:56:17 +0530

    triangle

--- code.cpp
+++ code.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout<<"helo world!";
+    int n =4;
+    for(int i=0;i<n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<" ";
+
+        }
+        for(int j=1;j<=(n-i);j++){
+
+        cout<<i+1;
+        }
+        cout<<endl;
+        
+    }
     return 0;
 }
-
 
