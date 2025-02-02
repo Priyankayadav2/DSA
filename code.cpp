26dba154f04c4e931cb11e0ace8e94ba0aa3b7a1
Priyankayadav2 <104354982+Priyankayadav2@users.noreply.github.com>
2025-02-02 18:42:45 +0530

    triangle

--- code.cpp
+++ code.cpp
@@ -2,18 +2,32 @@
 using namespace std;
 int main(){
     int n =4;
+    char ch='A';
     for(int i=0;i<n;i++){
         for(int j=1;j<=i;j++){
             cout<<" ";
 
         }
-        for(int j=1;j<=(n-i);j++){
-
-        cout<<i+1;
+        for (int j =1;j<=(n-i);j++){
+            cout<<ch;
+            
         }
+            ch=ch+1;
+
         cout<<endl;
-        
     }
+    // for(int i=0;i<n;i++){
+    //     for(int j=1;j<=i;j++){
+    //         cout<<" ";
+
+    //     }
+    //     for(int j=1;j<=(n-i);j++){
+
+    //     cout<<i+1;
+    //     }
+    //     cout<<endl;
+        
+    
     return 0;
 }
 
