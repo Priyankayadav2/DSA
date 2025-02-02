62342928be024fab3b1db22ca4fd17f39e6a3f72
Priyankayadav2 <104354982+Priyankayadav2@users.noreply.github.com>
2025-02-02 17:01:33 +0530

    flyod-triangle

--- code.cpp
+++ code.cpp
@@ -1,49 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-    // int n=4;
-    // for(int i=0;i<n;i++){
-    //     for(int j=0;j<(i+1);j++){
-    //         cout<<"* ";
-    //     }
-    // cout<<endl;
-    // int n=4;
-    // for(int i=0;i<n;i++){
-    //     for(int j=0;j<(i+1);j++){
-    //         cout<<(i+1)<<" ";
-    //     }
-    //     cout<<endl;
-    // }
-    // cout<<endl;
-    // int n=5;
-
-    // for(int i=0;i<n;i++){
-    //     char ch='A'+i;
-    //     for(int j=0;j<(i+1);j++){
-    //         cout<<ch<<" ";
-            
-    //     }
-
-    //     cout<<endl;
-    // }
-    // int n=4;
-    // for (int i = 0; i < n; i++) {
-    //     char ch = 'A';  // Start with 'A' for each row
-    //     for (int j = 0; j < (i + 1); j++) {
-    //         cout << ch << " ";
-    //         ch = ch + 1;  // Increment character
-    //     }
-    //     cout<<endl;
-    // }
-    int n = 4;
-    for(int i=0;i<n;i++){
-        for(int j = 1;j<=(i+1);j++){
-            cout<<j;
-        }
-        cout<<endl;
-
-    }
+    cout<<"helo";
 }
-
 
 
