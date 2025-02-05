fe786a394e8f992bc77fb3acfd88ba010581e3f0
Priyankayadav2 <104354982+Priyankayadav2@users.noreply.github.com>
2025-02-05 15:05:59 +0530

    passbyref

--- code.cpp
+++ code.cpp
@@ -1,27 +1,18 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int marks[]={23,21,67,45,90};
-    int n=5;
-    // marks[2]=88;
-    // cout<<marks[3]<<endl;
-    // cout<<marks[2]<<endl;
-    // cout<<sizeof(marks)<<endl;
+void changeArr(int arr[],int size){
+    cout<<"Changes in function (pass by reference) "<<endl;
+    for(int i =0;i<size;i++){
+        arr[i]=2*arr[i];
+    }
+}
 
-    
-    cout<<sizeof(marks)/sizeof(int)<<endl;
-    for(int i=0;i<n;i++){
-        cout<<marks[i]<<" ";
+int main(){
+    int arr[]={1,2,3};
+    changeArr(arr,3);
+    cout<<"Here is updated array pass by refernce   ";
+    for(int i =0;i<3;i++){
+        cout<<arr[i]<<" ";
     }
     cout<<endl;
-    int size=3;
-    int result[size];
-    cout<<"user input array ";
-    for (int i =0;i<size;i++){
-        cin>>result[i];
-    }
-    cout<<" another array is"<<endl;
-    for(int i=0;i<size;i++){
-        cout<<result[i]<<" "<<endl;
-    }
 }
