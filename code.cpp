a1aed5f0df3246a8e786cc6cc9134b9ff9ab3abc
Priyankayadav2 <104354982+Priyankayadav2@users.noreply.github.com>
2025-02-05 16:06:51 +0530

    reversearray

--- code.cpp
+++ code.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 using namespace std;
-int linearSearch(int arr[],int size,int target){
-    
-    for(int i =0;i<size;i++){
-        if(arr[i]==target){
-            return i;
-        }
-        
+void reverseArray(int arr[],int size){
+    int start=0;
+    int end=size-1;
+    while(start<end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
     }
-    return -1;
-
-    
 }
-
 int main(){
-    int arr[]={1,4,6,2,8,9,3};
-    int target =8;
-    cout<<  linearSearch(arr,7,target)<<endl;
-    return 0;
-}
+    int arr[]={2,4,1,6,8,3,2,9};
+    int size=8;//7
+    reverseArray(arr,size);
+    for(int i =0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    }
+
