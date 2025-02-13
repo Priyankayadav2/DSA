99e22257f843efa0533d6e598dee44d98020abc5
Priyankayadav2 <104354982+Priyankayadav2@users.noreply.github.com>
2025-02-13 18:17:59 +0530

    sumofsubarray

--- code.cpp
+++ code.cpp
@@ -6,16 +6,18 @@ using namespace std;
 int main(){
     int n=5;
     int arr[5]={1,2,3,4,5};
+    int maxSum=INT32_MIN;
+
     for(int start=0;start<n;start++){
+        int curSum=0;
         for(int end=start;end<n;end++){
-            for(int i=start;i<=end;i++){
-                cout<<arr[i];
+            curSum+=arr[end];
+            maxSum=max(curSum,maxSum);
             }
-            cout<<" ";
         }
-        cout<<endl;
-    }
+        cout<<"Max subarray Sum = "<<maxSum<<endl;
+    
     return 0;
-    }
+}
     
 
