15ebc85cdb6e2bcd2bcc6c3e3f7ab7e62f4056d0
Priyankayadav2 <104354982+Priyankayadav2@users.noreply.github.com>
2025-02-13 17:20:45 +0530

    subaaray of given arary

--- code.cpp
+++ code.cpp
@@ -1,28 +1,19 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
-int linearSearch(const vector<int> & vec,int target){
-    for(int i =0;i<vec.size();i++){
-        if(vec[i]==target){
-            return i;
-        }
-    }
-    return -1;
-}
-int main(){
-    cout <<"Linear search on vector"<<endl;
-    vector <int> vec={10,25,30,45,50,75};
-    int target;
-    cout<<"Enter the target number to serach :";
-    cin>>target;
 
-    int index=linearSearch(vec,target);
-    if(index!=-1){
-        cout<<"Element found at index "<<index<<endl;
-    }
-    else{
-        cout<<"element not found !"<<endl;
+
+int main(){
+    int n=5;
+    int arr[5]={1,2,3,4,5};
+    for(int start=0;start<n;start++){
+        for(int end=start;end<n;end++){
+            for(int i=start;i<=end;i++){
+                cout<<arr[i];
+            }
+            cout<<" ";
+        }
+        cout<<endl;
     }
     return 0;
     }
