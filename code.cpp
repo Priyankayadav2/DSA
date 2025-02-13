35bf010869947c58b72444b388df194ae97467f1
Priyankayadav2 <104354982+Priyankayadav2@users.noreply.github.com>
2025-02-13 15:18:09 +0530

    vector

--- code.cpp
+++ code.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 int main(){
-    cout <<"vector";
+    cout <<"vector"<<endl;
+    // vector <int> vec={1,2,3};
+    // cout<<vec[0];
+
+    // vector <int>vec(5,0);
+    // for(int i :vec){
+    //     cout<<i<<endl;
+    // }
+    //for characters
+    // vector<char> vec={'a','b','c','d'};
+    // for (char i:vec){
+    //     cout<<i<<endl;
+    // }
+    
     return 0;
     }
     
